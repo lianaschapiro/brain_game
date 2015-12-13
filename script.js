747069ec44a5786455e86c88dd093be905f6eabe
lianaschapiro <liana.schapiro@gmail.com>
2015-12-13 11:18:17 -0500

    Added submit function with preventDefault and 'clear input box' functionality

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
+ 	$("form").submit(function(e){
+        e.preventDefault();
+       $("input").val("");
 
+	});
 
 });
+
+// });
+// if what is entered matches the value of the attr "alt" of the input, 
+// 		alert that says "you are correct"
+// 		change the form html to be a paragraph with the value typed
+// 
+// 		else alert that says try again
+// 		form clears back to the placeholder
+// 
+
+
